386e04a5460eca32c2c38a44bdf6244eebeeeffc
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2023-08-10 18:07:05 -0700

    React redux add and initial page Move Recover Fuel routes

--- src/App.js
+++ src/App.js
@@ -3,27 +3,36 @@ import Recovery from './components/Recovery';
 import Fuel from './components/Fuel';
 import Move from './components/Move';
 
+import { Provider } from 'react-redux';
+import store from './utils/store';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <div>
-            <h1>Welcome to Your Wellness App</h1>
-            {/* Add your components and routes here */}
-          </div>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store = {store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <div>
+              <h1>Welcome to Your Wellness App</h1>
+              <Route exact path="/Move" component={Move} />
+              <Route exact path="/Recovery" component={Recovery} />
+              <Route exact path="/Fuel" component={Fuel} />
+              {/* Add your components and routes here */}
+            </div>
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Provider>
+    
   );
 }
 
