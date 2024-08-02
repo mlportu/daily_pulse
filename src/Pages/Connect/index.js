6f6f387931c6f926465180e3882983b91db91311
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2024-08-02 14:10:34 -0400

    updated connect page

--- src/Pages/Connect/index.js
+++ src/Pages/Connect/index.js
@@ -8,6 +8,7 @@ import { ImageList, ImageListItem,ImageListItemBar, ListItemIcon, ThemeProvider,
 import SpeedSharpIcon from '@mui/icons-material/SpeedSharp';
 import DirectionsRunTwoToneIcon from '@mui/icons-material/DirectionsRunTwoTone';
 import SyncTwoToneIcon from '@mui/icons-material/SyncTwoTone';
+import { spacing } from '@mui/system';
 
 
 const Connect = () => {
@@ -17,8 +18,7 @@ const Connect = () => {
             width:'100%',
             display: 'flex', 
             flexDirection: 'column',
-            padding: '20px',
-            backgroundColor: '#000000',
+            backgroundColor: '#F808C8',
         },
         header: {
             width: '100%',
@@ -38,183 +38,184 @@ const Connect = () => {
 
   return (
     <Box>
-        <Box>
-            <Box sx={connectStyles.header}>
-                 <SyncTwoToneIcon/>
+     <Grid container spacing={2}>
+          {/* First row: Full width */}
+          <Grid item xs={12}>
+            <Paper style={{ padding: 20 }} align ="center"> 
+                <SyncTwoToneIcon/>
                 Fuel
-            </Box>
-            <Box>
-                <Grid  sx={connectStyles.connectionItem} container spacing={2}>
-                    <Grid  item xs={6} md={3}>
-                        <ImageList>
-                                <ImageListItem item xs={6} md={4}>
-                                    <img
-                                        srcSet='https://1000logos.net/wp-content/uploads/2017/08/Logo-Kroger.png'
-                                        src='https://1000logos.net/wp-content/uploads/2017/08/Logo-Kroger.png'
-                                        alt="Kroger"
-                                    />
-                                </ImageListItem>
-                        </ImageList>
-                    </Grid>
-                    <Grid item xs={6} md={3}>
-                        <ImageList  sx={{ width: 1, textAlign: 'center' }}>
-                                <ImageListItem item xs={6} md={4}>
-                                    <img
-                                        srcSet='https://assets.leads-iq.com/assets/img/cmdea0e1f4a-f5de-4ee3-b0f7-e0db6857db2c.png'
-                                        src='https://assets.leads-iq.com/assets/img/cmdea0e1f4a-f5de-4ee3-b0f7-e0db6857db2c.png'
-                                        alt="Factor"
-                                    />
-                                </ImageListItem>
-                        </ImageList>
-                    </Grid>
-                    <Grid item xs={6} md={3}>
-                        <ImageList sx={{ width: 1, textAlign: 'center' }}>
-                                <ImageListItem item xs={6} md={4}>
-                                    <img
-                                        srcSet='https://i.pcmag.com/imagery/reviews/02QLfUYNhLSOZYYjHQ8jLQQ-7..v1635288397.png'
-                                        src='https://i.pcmag.com/imagery/reviews/02QLfUYNhLSOZYYjHQ8jLQQ-7..v1635288397.png'
-                                        alt="Trifecta"
-                                    />
-                                </ImageListItem>
-                        </ImageList>
-                    </Grid>
-                    <Grid item xs={6} md={3}>
-                        <ImageList sx={{ width: 1,  }}>
-                                <ImageListItem item xs={6} md={4} textAlign='center'>
-                                    <img
-                                        srcSet='https://images.squarespace-cdn.com/content/v1/5d97716aeb25db6ebdc07f9f/1580686095119-IR2C8YAJMCU4JYYFZRSV/hellofresh-hlogo-MatthewPeters.png'
-                                        src='https://images.squarespace-cdn.com/content/v1/5d97716aeb25db6ebdc07f9f/1580686095119-IR2C8YAJMCU4JYYFZRSV/hellofresh-hlogo-MatthewPeters.png'
-                                        alt="Hello Fresh"
-                                    />
-                                </ImageListItem>
-                        </ImageList>
-                    </Grid>
-                </Grid>
-            </Box>
-        </Box>
-        <Box sx={{boarderRadius: 1, m:2}}>
-            <Box sx={{ width: 1, m:2 }}>
-                <Box>
-                    <DirectionsRunTwoToneIcon/>
-                    Move
-                </Box>
-            </Box>
-            <Box sx={{ width: 1, m:2 }}>
-                <Grid container spacing={2}>    
-                    <Grid item xs={6} md={4}>
-                        <ImageList sx={{ width: 1 }}>
-                                <ImageListItem item xs={6} md={4}>
-                                    <img
-                                        srcSet='https://m.media-amazon.com/images/S/stores-image-uploads-na-prod/5/AmazonStores/ATVPDKIKX0DER/f0873b00db48ac8dab1e74822d4314ee.w1500.h400.png'
-                                        src='https://m.media-amazon.com/images/S/stores-image-uploads-na-prod/5/AmazonStores/ATVPDKIKX0DER/f0873b00db48ac8dab1e74822d4314ee.w1500.h400.png'
-                                        alt="Whoop"
-                                    />
-                                </ImageListItem>
-                        </ImageList>
-                    </Grid>
-                    <Grid item xs={6} md={4}>
-                        <ImageList sx={{ width: 1 }}>
-                                <ImageListItem item xs={6} md={4}>
-                                    <img
-                                        srcSet='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Apple_Watch_official_logo.svg/2560px-Apple_Watch_official_logo.svg.png'
-                                        src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Apple_Watch_official_logo.svg/2560px-Apple_Watch_official_logo.svg.png'
-                                        alt="Apple Watch"
-                                    />
-                                </ImageListItem>
-                        </ImageList>
-                    </Grid>
-                    <Grid item xs={6} md={4}>
-                        <ImageList sx={{ width: 1 }}>
-                                <ImageListItem item xs={6} md={4}>
-                                    <img
-                                        srcSet='https://seeklogo.com/images/Q/qura-ring-logo-D4FFD69DBC-seeklogo.com.png'
-                                        src='https://seeklogo.com/images/Q/qura-ring-logo-D4FFD69DBC-seeklogo.com.png'
-                                        alt="Oura Ring"
-                                    />
-                                </ImageListItem>
-                        </ImageList>
-                    </Grid>
-                    <Grid item xs={6} md={4}>
-                        <ImageList sx={{ width: 1 }}>
-                                <ImageListItem item xs={6} md={4}>
-                                    <img
-                                        srcSet='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Fitbit_logo.svg/2560px-Fitbit_logo.svg.png'
-                                        src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Fitbit_logo.svg/2560px-Fitbit_logo.svg.png'
-                                        alt="Fitbit"
-                                    />
-                                </ImageListItem>
-                        </ImageList>
-                    </Grid>
-                    <Grid item xs={6} md={4}>
-                        <ImageList sx={{ width: 1 }}>
-                                <ImageListItem item xs={6} md={4}>
-                                    <img
-                                        srcSet='https://upload.wikimedia.org/wikipedia/commons/8/8e/IFIT_Health_%26_Fitness_logo.jpg'
-                                        src='https://upload.wikimedia.org/wikipedia/commons/8/8e/IFIT_Health_%26_Fitness_logo.jpg'
-                                        alt="iFit"
-                                    />
-                                </ImageListItem>
-                        </ImageList>
-                    </Grid>
-                </Grid>
-            </Box>
-        </Box>
-        <Box sx={{boarderRadius: 1, m:2}}>
-             <Box sx={{ width: 1, m:2 }}>
-                <Box>
-                    <SpeedSharpIcon/>
-                    Recover
-                </Box>
-            </Box>
-            <Box sx={{ width: 1, m:2 }}>
-                <Grid container spacing={2}>    
-                    <Grid item xs={6} md={3}>
-                        <ImageList sx={{ width: 1 }}>
-                                <ImageListItem item xs={6} md={4}>
-                                    <img
-                                        srcSet='https://m.media-amazon.com/images/S/stores-image-uploads-na-prod/5/AmazonStores/ATVPDKIKX0DER/f0873b00db48ac8dab1e74822d4314ee.w1500.h400.png'
-                                        src='https://m.media-amazon.com/images/S/stores-image-uploads-na-prod/5/AmazonStores/ATVPDKIKX0DER/f0873b00db48ac8dab1e74822d4314ee.w1500.h400.png'
-                                        alt="Whoop"
-                                    />
-                                </ImageListItem>
-                        </ImageList>
-                    </Grid>
-                    <Grid item xs={6} md={3}>
-                        <ImageList sx={{ width: 1 }}>
-                                <ImageListItem item xs={6} md={4}>
-                                    <img
-                                        srcSet='https://bedtimesmagazine.com/wp-content/uploads/2023/05/Sleep_Number-Logo.wine_-1.png'
-                                        src='https://bedtimesmagazine.com/wp-content/uploads/2023/05/Sleep_Number-Logo.wine_-1.png'
-                                        alt="Sleep Number"
-                                    />
-                                </ImageListItem>
-                        </ImageList>
-                    </Grid>
-                    <Grid item xs={6} md={3}>
-                        <ImageList sx={{ width: 1 }}>
-                                <ImageListItem item xs={6} md={4}>
-                                    <img
-                                        srcSet='https://img.coupert.com/ccsimg/8fd6211e7d07ec2aa73aef615d44c8c3.webp'
-                                        src='https://img.coupert.com/ccsimg/8fd6211e7d07ec2aa73aef615d44c8c3.webp'
-                                        alt="Yoga Body"
-                                    />
-                                </ImageListItem>
-                        </ImageList>
-                    </Grid>
-                    <Grid item xs={6} md={3}>
-                        <ImageList sx={{ width: 1 }}>
-                                <ImageListItem item xs={6} md={4}>
-                                    <img
-                                        srcSet='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfNxR0wonS2mwXvj7EVYrAppoiUUo7DkYndg&s'
-                                        src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfNxR0wonS2mwXvj7EVYrAppoiUUo7DkYndg&s'
-                                        alt="Pliability"
-                                    />
-                                </ImageListItem>
-                        </ImageList>
-                    </Grid>
-                </Grid>
-            </Box>
-        </Box>
+            </Paper>
+          </Grid>
+
+          {/* Second row: Four columns */}
+          <Grid item xs={12} sm={6} md={3}>
+            <Paper style={{ padding: 20 }}>
+                <ImageListItem item xs={6} md={4}>
+                    <img
+                            srcSet='https://1000logos.net/wp-content/uploads/2017/08/Logo-Kroger.png'
+                            src='https://1000logos.net/wp-content/uploads/2017/08/Logo-Kroger.png'
+                            alt="Kroger"
+                        />
+                </ImageListItem>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <Paper style={{ padding: 20 }}>
+                <ImageListItem item xs={6} md={4}>
+                    <img
+                        srcSet='https://assets.leads-iq.com/assets/img/cmdea0e1f4a-f5de-4ee3-b0f7-e0db6857db2c.png'
+                        src='https://assets.leads-iq.com/assets/img/cmdea0e1f4a-f5de-4ee3-b0f7-e0db6857db2c.png'
+                        alt="Factor"
+                    />
+                </ImageListItem>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <Paper style={{ padding: 20 }}>
+                <ImageListItem item xs={6} md={4}>
+                    <img
+                        srcSet='https://i.pcmag.com/imagery/reviews/02QLfUYNhLSOZYYjHQ8jLQQ-7..v1635288397.png'
+                        src='https://i.pcmag.com/imagery/reviews/02QLfUYNhLSOZYYjHQ8jLQQ-7..v1635288397.png'
+                        alt="Trifecta"
+                    />
+                </ImageListItem>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <Paper style={{ padding: 20 }}>
+                <ImageListItem item xs={6} md={4} textAlign='center'>
+                    <img
+                        srcSet='https://images.squarespace-cdn.com/content/v1/5d97716aeb25db6ebdc07f9f/1580686095119-IR2C8YAJMCU4JYYFZRSV/hellofresh-hlogo-MatthewPeters.png'
+                        src='https://images.squarespace-cdn.com/content/v1/5d97716aeb25db6ebdc07f9f/1580686095119-IR2C8YAJMCU4JYYFZRSV/hellofresh-hlogo-MatthewPeters.png'
+                        alt="Hello Fresh"
+                    />
+                </ImageListItem>
+            </Paper>
+          </Grid>
+        </Grid>
+     <Grid container spacing={2}>
+        {/* First row: Full width */}
+        <Grid item xs={12}>
+            <Paper style={{ padding: 20 }} align="center"> 
+                <DirectionsRunTwoToneIcon/>
+                Move
+            </Paper>
+        </Grid>
+
+          {/* Second row: Four columns */}
+          <Grid item xs={12} sm={6} md={2.4}>
+            <Paper style={{ padding: 20 }}>
+                <ImageListItem item xs={6} md={4}>
+                     <img
+                        srcSet='https://m.media-amazon.com/images/S/stores-image-uploads-na-prod/5/AmazonStores/ATVPDKIKX0DER/f0873b00db48ac8dab1e74822d4314ee.w1500.h400.png'
+                        src='https://m.media-amazon.com/images/S/stores-image-uploads-na-prod/5/AmazonStores/ATVPDKIKX0DER/f0873b00db48ac8dab1e74822d4314ee.w1500.h400.png'
+                        alt="Whoop"
+                    />
+                </ImageListItem>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6} md={2.4}>
+            <Paper style={{ padding: 20 }}>
+                <ImageListItem item xs={6} md={4}>
+                    <img
+                        srcSet='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Apple_Watch_official_logo.svg/2560px-Apple_Watch_official_logo.svg.png'
+                        src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Apple_Watch_official_logo.svg/2560px-Apple_Watch_official_logo.svg.png'
+                        alt="Apple Watch"
+                    />
+                </ImageListItem>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6} md={2.4}>
+            <Paper style={{ padding: 20 }}>
+                <ImageListItem item xs={6} md={4}>
+                    <img
+                        srcSet='https://seeklogo.com/images/Q/qura-ring-logo-D4FFD69DBC-seeklogo.com.png'
+                        src='https://seeklogo.com/images/Q/qura-ring-logo-D4FFD69DBC-seeklogo.com.png'
+                        alt="Oura Ring"
+                    />
+                </ImageListItem>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6} md={2.4}>
+            <Paper style={{ padding: 20 }}>
+                <ImageListItem item xs={6} md={4}>
+                    <img
+                        srcSet='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Fitbit_logo.svg/2560px-Fitbit_logo.svg.png'
+                        src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Fitbit_logo.svg/2560px-Fitbit_logo.svg.png'
+                        alt="Fitbit"
+                    />
+                </ImageListItem>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6} md={2.4}>
+            <Paper style={{ padding: 20 }}>
+                <ImageListItem item xs={6} md={4}>
+                    <img
+                        srcSet='https://upload.wikimedia.org/wikipedia/commons/8/8e/IFIT_Health_%26_Fitness_logo.jpg'
+                        src='https://upload.wikimedia.org/wikipedia/commons/8/8e/IFIT_Health_%26_Fitness_logo.jpg'
+                        alt="iFit"
+                    />  
+                </ImageListItem>
+            </Paper>
+          </Grid>
+        </Grid>   
+        <Grid container spacing={2}>
+          {/* First row: Full width */}
+          <Grid item xs={12}>
+            <Paper style={{ padding: 20 }} align="center"> 
+                <SpeedSharpIcon/>
+                Recover
+            </Paper>
+          </Grid>
+
+          {/* Second row: Four columns */}
+          <Grid item xs={12} sm={6} md={3}>
+            <Paper style={{ padding: 20 }}>
+                <ImageListItem item xs={6} md={4}>
+                    <img
+                        srcSet='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfNxR0wonS2mwXvj7EVYrAppoiUUo7DkYndg&s'
+                        src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfNxR0wonS2mwXvj7EVYrAppoiUUo7DkYndg&s'
+                        alt="Pliability"
+                    />  
+                </ImageListItem>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <Paper style={{ padding: 20 }}>
+                <ImageListItem item xs={6} md={4}>
+                    <img
+                        srcSet='https://img.coupert.com/ccsimg/8fd6211e7d07ec2aa73aef615d44c8c3.webp'
+                        src='https://img.coupert.com/ccsimg/8fd6211e7d07ec2aa73aef615d44c8c3.webp'
+                        alt="Yoga Body"
+                    />
+                </ImageListItem>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <Paper style={{ padding: 20 }}>
+                <ImageListItem item xs={6} md={4}>
+                    <img
+                        srcSet='https://bedtimesmagazine.com/wp-content/uploads/2023/05/Sleep_Number-Logo.wine_-1.png'
+                        src='https://bedtimesmagazine.com/wp-content/uploads/2023/05/Sleep_Number-Logo.wine_-1.png'
+                        alt="Sleep Number"
+                    />
+                </ImageListItem>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <Paper style={{ padding: 20 }}>
+                <ImageListItem item xs={6} md={4} textAlign='center'>
+                    <img
+                        srcSet='https://m.media-amazon.com/images/S/stores-image-uploads-na-prod/5/AmazonStores/ATVPDKIKX0DER/f0873b00db48ac8dab1e74822d4314ee.w1500.h400.png'
+                        src='https://m.media-amazon.com/images/S/stores-image-uploads-na-prod/5/AmazonStores/ATVPDKIKX0DER/f0873b00db48ac8dab1e74822d4314ee.w1500.h400.png'
+                        alt="Whoop"
+                    />
+                </ImageListItem>
+            </Paper>
+          </Grid>
+        </Grid>
+         
+
     </Box>
   );
 };
--- src/Pages/Recover/index.js
+++ src/Pages/Recover/index.js
@@ -1,13 +1,109 @@
 import React from 'react';
-import Grid from '@mui/material/Grid';
+import { AppBar, Toolbar, Typography, Container, Grid, Paper, Button, Link, Card, CardMedia, CardContent } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+// Custom styles
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    marginBottom: theme.spacing(4),
+    backgroundColor: '#2E7D32', // Earthy green tone
+  },
+  heroImage: {
+    height: 400,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  section: {
+    padding: theme.spacing(4, 0),
+    backgroundColor: '#E3F2FD', // Light blue sky tone
+  },
+  card: {
+    maxWidth: 345,
+    margin: 'auto',
+  },
+  cardMedia: {
+    height: 200,
+  },
+  cardContent: {
+    backgroundColor: '#A5D6A7', // Light green tone
+  },
+  footer: {
+    marginTop: theme.spacing(4),
+    padding: theme.spacing(2),
+    backgroundColor: '#2E7D32', // Earthy green tone
+    color: 'white',
+  },
+}));
 
 const Recover = () => {
+  const classes = useStyles();
+
   return (
-    <Grid >
-      <h2>Recovery</h2>
-      <p>This is where all the recovery information from various APIs will go</p>
-      {/* Add your recovery-related content and functionality here */}
-    </Grid>
+    <div>
+      <AppBar position="static" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6">Relaxation & Recovery</Typography>
+        </Toolbar>
+      </AppBar>
+      
+      <div
+        className={classes.heroImage}
+        style={{ backgroundImage: 'url(https://source.unsplash.com/featured/?nature,relaxation)' }}
+      >
+        <Container>
+          <Typography variant="h3" align="center" style={{ color: 'white', paddingTop: '150px' }}>
+            Embrace the Healing Power of Nature
+          </Typography>
+        </Container>
+      </div>
+      
+      <Container className={classes.section}>
+        <Typography variant="h4" align="center" gutterBottom>
+          Intentional Recovery
+        </Typography>
+        <Typography variant="body1" align="center" paragraph>
+          Recovery is a core part of a healthy lifestyle and is critical for well-being when paired with exercise, movement, and a balanced diet. Explore our resources to help you relax and rejuvenate.
+        </Typography>
+        
+        <Grid container spacing={4}>
+          {[1, 2, 3, 4].map((index) => (
+            <Grid item xs={12} sm={6} md={3} key={index}>
+              <Card className={classes.card}>
+                <CardMedia
+                  className={classes.cardMedia}
+                  image={`https://source.unsplash.com/featured/?relaxation,${index}`}
+                  title="Relaxation Image"
+                />
+                <CardContent className={classes.cardContent}>
+                  <Typography variant="h6">Recovery Activity {index}</Typography>
+                  <Typography variant="body2">
+                    Discover ways to unwind and restore your energy with our recommended activities.
+                  </Typography>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    fullWidth
+                    component={Link}
+                    href="#"
+                    style={{ marginTop: '10px' }}
+                  >
+                    Learn More
+                  </Button>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+      
+      <footer className={classes.footer}>
+        <Container>
+          <Typography variant="body1" align="center">
+            &copy; 2024 Relaxation & Recovery. All rights reserved.
+          </Typography>
+        </Container>
+      </footer>
+    </div>
   );
 };
 
